74f7184b3fb4e90d1b3f9de618adcf3e35a463c8
ProWega <deriswerderis@gmail.com>
2024-05-25 11:34:45 +0300

    Первое задание

--- fa/program.cs
+++ fa/program.cs
@@ -6,52 +6,104 @@ using System.Threading.Tasks;
 
 namespace fans
 {
-  public class State
-  {
-    public string Name;
-    public Dictionary<char, State> Transitions;
-    public bool IsAcceptState;
-  }
+    public class State
+    {
+        public string Name;
+        public Dictionary<char, State> Transitions;
+        public bool IsAcceptState;
+    }
 
 
-  public class FA1
-  {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
-  }
 
-  public class FA2
-  {
-    public bool? Run(IEnumerable<char> s)
+    public class FA1
     {
-      return false;
-    }
-  }
-  
-  public class FA3
-  {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
-  }
+        public State q0 = new State()
+        {
+            Name = "q0",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q1 = new State()
+        {
+            Name = "q1",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q2 = new State()
+        {
+            Name = "q2",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q3 = new State()
+        {
+            Name = "q3",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
 
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      String s = "01111";
-      FA1 fa1 = new FA1();
-      bool? result1 = fa1.Run(s);
-      Console.WriteLine(result1);
-      FA2 fa2 = new FA2();
-      bool? result2 = fa2.Run(s);
-      Console.WriteLine(result2);
-      FA3 fa3 = new FA3();
-      bool? result3 = fa3.Run(s);
-      Console.WriteLine(result3);
+        State InitialState;
+
+        public FA1()
+        {
+            InitialState = q0;
+
+            q0.Transitions['0'] = q3;
+            q0.Transitions['1'] = q0;
+
+            q3.Transitions['0'] = q2;
+            q3.Transitions['1'] = q1;
+
+            q1.Transitions['0'] = q2;
+            q1.Transitions['1'] = q1;
+
+            q2.Transitions['0'] = q2;
+            q2.Transitions['1'] = q2;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s)
+            {
+                if (!current.Transitions.ContainsKey(c)) return null;
+                current = current.Transitions[c];
+            }
+            return current.IsAcceptState;
+        }
     }
-  }
+
+
+    public class FA2
+    {
+        public bool? Run(IEnumerable<char> s)
+        {
+            return false;
+        }
+    }
+
+    public class FA3
+    {
+        public bool? Run(IEnumerable<char> s)
+        {
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "01111";
+            FA1 fa1 = new FA1();
+            bool? result1 = fa1.Run(s);
+            Console.WriteLine(result1);
+            FA2 fa2 = new FA2();
+            bool? result2 = fa2.Run(s);
+            Console.WriteLine(result2);
+            FA3 fa3 = new FA3();
+            bool? result3 = fa3.Run(s);
+            Console.WriteLine(result3);
+        }
+    }
 }

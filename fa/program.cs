946b786a2f8273ecfbd372214f1fd421c724bb9d
ProWega <deriswerderis@gmail.com>
2024-05-25 12:14:36 +0300

    2 и 3 задание

--- fa/program.cs
+++ fa/program.cs
@@ -13,8 +13,6 @@ namespace fans
         public bool IsAcceptState;
     }
 
-
-
     public class FA1
     {
         public State q0 = new State()
@@ -76,17 +74,108 @@ namespace fans
 
     public class FA2
     {
+        public State q0 = new State()
+        {
+            Name = "q0",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q1 = new State()
+        {
+            Name = "q1",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q2 = new State()
+        {
+            Name = "q2",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q3 = new State()
+        {
+            Name = "q3",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState;
+
+        public FA2()
+        {
+            InitialState = q0;
+
+            q0.Transitions['0'] = q1;
+            q0.Transitions['1'] = q2;
+
+            q1.Transitions['0'] = q0;
+            q1.Transitions['1'] = q3;
+
+            q2.Transitions['0'] = q3;
+            q2.Transitions['1'] = q0;
+
+            q3.Transitions['0'] = q2;
+            q3.Transitions['1'] = q1;
+        }
+
         public bool? Run(IEnumerable<char> s)
         {
-            return false;
+            State current = InitialState;
+            foreach (var c in s)
+            {
+                if (!current.Transitions.ContainsKey(c)) return null;
+                current = current.Transitions[c];
+            }
+            return current.IsAcceptState;
         }
     }
 
     public class FA3
     {
+        public State q0 = new State()
+        {
+            Name = "q0",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q1 = new State()
+        {
+            Name = "q1",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State q2 = new State()
+        {
+            Name = "q2",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState;
+
+        public FA3()
+        {
+            InitialState = q0;
+
+            q0.Transitions['0'] = q0;
+            q0.Transitions['1'] = q1;
+
+            q1.Transitions['0'] = q0;
+            q1.Transitions['1'] = q2;
+
+            q2.Transitions['0'] = q2;
+            q2.Transitions['1'] = q2;
+        }
+
         public bool? Run(IEnumerable<char> s)
         {
-            return false;
+            State current = InitialState;
+            foreach (var c in s)
+            {
+                if (!current.Transitions.ContainsKey(c)) return null;
+                current = current.Transitions[c];
+            }
+            return current.IsAcceptState;
         }
     }
 
@@ -94,7 +183,7 @@ namespace fans
     {
         static void Main(string[] args)
         {
-            String s = "01111";
+            String s = "101111";
             FA1 fa1 = new FA1();
             bool? result1 = fa1.Run(s);
             Console.WriteLine(result1);
